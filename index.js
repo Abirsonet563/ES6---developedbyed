b4d8fe31606ec73f46c94f7076356a59f2834bd7
Abir Hasan <abirsonet563@gmail.com>
2024-11-01 19:42:29 +0000

    Tilt Concept

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 //OLD JAVASCRIPT
+var name = "Abir";
 
-const name = ['Abir', 'Hasan', 'Saif']
-for (i = 1; i < name.length; i++){
-    console.log(name);
-}
+console.log("My Name Is " + name + ".")
+
 
 //ES6
-const nameES6 = ['Abir', 'Hasan', 'Saif']
-for(i = 0; i < nameES6.length; i++){
-    
-    console.log(nameES6);
-}
+
+var nameES6 = "Abir Hasan"
+
+console.log(`My name is ${nameES6}`);
