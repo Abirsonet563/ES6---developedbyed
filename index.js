4119bcaf6a26f5f2bb052a85e03243f69e660e71
Abir Hasan <abirsonet563@gmail.com>
2024-11-01 19:25:26 +0000

    Assign Variable CONST

---
+++ index.js
@@ -0,0 +1,18 @@
+//OLD JAVASCRIPT
+var counter = 5;
+
+function sayName(){
+   var name = "Abir"
+   name = "Abir Hasan"
+   console.log(name);
+}
+sayName();
+
+//ES6
+
+function sayNameES6(){
+    const name2 = "Abir"
+    name2 = "Abir Hasan"
+    console.log(name2);
+}
+sayNameES6();
