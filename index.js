656df548411d45b326bfeb7a3e1b7c84ee61de93
Abir Hasan <abirsonet563@gmail.com>
2024-11-16 19:32:44 +0000

    Pointer and Arrow Function

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 //OLD JAVASCRIPT
-
-function sayName(){
-    console.log("My name is Abir");
-}
-
-var sayAge = function(){
-    console.log("Age is Just a Number");
-}
-sayAge();
-sayName();
+var user = {
+    name: "Abir",
+    age: 23,
+    sayName: function(){
+        console.log(`My Name is ${this.name}`)
+        var that = this;
+        var fullName = function(){
+            console.log(`My name is ${that.name} and My Age is ${that.age}`); 
+        }
+        fullName();
+    }
+};
+user.sayName();
 
 //ES6
 
-const myLocation = () => {
-    console.log("My Location is Unknown");
-};
-myLocation();
-
-const myLocationTwo = location => {
-    console.log(`My Location is ${location}`);
+var userTwo = {
+    name: 'Abir',
+    age: 23,
+    sayNameTwo: function(){
+        console.log(`My name is ${this.name}`);
+        const fullNameTwo = () => {
+            console.log(`My Name is ${this.name} and My Age is ${this.age}`);
+        }
+        fullNameTwo();
+    }
 }
-myLocationTwo("Paris");
-
-const myLocationThree = location => 
-    console.log(`My Location is ${location}`);
-//Without bracket/parenthesis when only one parameter 
-myLocationThree("London")
+userTwo.sayNameTwo();
