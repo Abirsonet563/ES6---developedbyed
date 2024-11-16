3f5191bdf807249e73214c8db4794a759daca721
Abir Hasan <abirsonet563@gmail.com>
2024-11-16 22:48:11 +0000

    Class, Constructor, Super

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 //OLD JAVASCRIPT
 
+function person(name, age, hairColor){
+ this.name= name;
+ this.age= age;
+ this.hairColor= hairColor;
+}
 
+person.prototype.sayName = function() {
+    console.log(`My name is ${this.name}`);
+};
 
+function Abir(ocupation, hobby, name, age, hairColor){
+person.call(this, name, age, hairColor);
+this.ocupation = ocupation;
+this.hobby = hobby;
+}
+
+Abir.prototype = Object.create(person.prototype);
+
+const Person = new Abir ("SE","Coding","Abir",23,"Black")
+const saif = new person("Saif",19,"Black")
+console.log(saif);
+console.log(Person);
+Person.sayName();
 //ES6
 
-shoppingList = ["Cow","Cat","Milk"];
+class shoppingListTwo{
+    constructor(item, nr){
+    this.item = item;
+    this.nr = nr;
+}
+ sayList(){
+    console.log(this.item);
+ }
+}
 
-//forEach Loop {Just Go Through Each Items}
-shoppingList.forEach(product =(product, index)=>{
-    console.log(`Product is ${product} and Index is ${index} +  Nice Choice`)
-});
 
-//Map 
-const shoppingListTwo = shoppingList.map(product =>{
-    return console.log(`Product is ${product} New`)
-})
-
-const shoppingListThree = shoppingList.map(product => console.log(`Product is ${product} Very New`))
-
-const filter = shoppingList.filter(item =>{
-    return item === "Cat";
-});
-console.log(filter);
-
-const filterListTwo = shoppingList.filter(item => item !== "Cat")
-console.log(filterListTwo)
+class product extends shoppingListTwo{
+    constructor(item, nr, amount, cost){
+        super(item, nr);
+        this.amount = amount;
+        this.cost = cost;
+    }
+}
+const newShoppingList = new product(['Milk','Cow','Elefent'],3,5,120)
+console.log(newShoppingList);
+newShoppingList.sayList();
