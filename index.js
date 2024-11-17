67acfa9704b2bdfff62f69d9af96d466fcd2bd97
Abir Hasan <abirsonet563@gmail.com>
2024-11-17 08:46:23 +0000

    Promise - async

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
 //OLD JAVASCRIPT
 
-function person(name, age, hairColor){
- this.name= name;
- this.age= age;
- this.hairColor= hairColor;
+console.log("Start");
+function getData(data, callback){
+    setTimeout(() => {
+        console.log("Reading From DataBase")
+        callback({data: data})
+    }, 2000);
 }
-
-person.prototype.sayName = function() {
-    console.log(`My name is ${this.name}`);
-};
-
-function Abir(ocupation, hobby, name, age, hairColor){
-person.call(this, name, age, hairColor);
-this.ocupation = ocupation;
-this.hobby = hobby;
-}
-
-Abir.prototype = Object.create(person.prototype);
-
-const Person = new Abir ("SE","Coding","Abir",23,"Black")
-const saif = new person("Saif",19,"Black")
-console.log(saif);
-console.log(Person);
-Person.sayName();
+getData(5, function(data) {
+    console.log(data);
+});
+console.log("Finish");
 //ES6
-
-class shoppingListTwo{
-    constructor(item, nr){
-    this.item = item;
-    this.nr = nr;
-}
- sayList(){
-    console.log(this.item);
- }
-}
-
-
-class product extends shoppingListTwo{
-    constructor(item, nr, amount, cost){
-        super(item, nr);
-        this.amount = amount;
-        this.cost = cost;
-    }
-}
-const newShoppingList = new product(['Milk','Cow','Elefent'],3,5,120)
-console.log(newShoppingList);
-newShoppingList.sayList();
+const prom = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        // resolve({user: "Abir", pass: 123});
+        reject(new Error("Something Went Wrong"));
+    }, 1000)
+});
+   prom.then(data => {
+    console.log(data);
+   })
+   .catch(err => console.log(err));
